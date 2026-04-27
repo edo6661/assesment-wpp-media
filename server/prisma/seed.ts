034c94b7ac1305285be10df1573d05ce118b3714
edawg <muhammadridho5654@gmail.com>
2026-04-28 00:13:40 +0700

    chore: mysql to posgres

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -88,7 +88,6 @@ async function main() {
 main()
   .catch((e) => {
     console.error("❌ Error seeding data:", e);
-    process.exit(1);
   })
   .finally(async () => {
     await prisma.$disconnect();
